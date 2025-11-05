214bb2ed7923b57b3862402479bf06cd8c66af3d
uncitxfoundation-org <uncitxfoundation@gmail.com>
2025-11-05 13:21:57 +0530

    Add all NGO Section in Homepage

---
+++ projects-section.js
@@ -0,0 +1,6 @@
+// Projects Section Specific JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+  // Add any projects section specific JavaScript here
+  console.log('Projects section loaded');
+});
+
